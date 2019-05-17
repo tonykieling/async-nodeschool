9bd2a83f387496933a7a459f49da96d308b37114
Tony Kieling <tony.kieling@gmail.com>
2019-05-17 09:01:51 -0700

    key on .env

--- currency.js
+++ currency.js
@@ -1,5 +1,6 @@
 // https://medium.freecodecamp.org/how-to-master-async-await-with-this-real-world-example-19107e7558ad
-const myKey = "4200f01792899473248cd9bff4c9c780"
+
+const myKey = ""
 
 getCurrency = async (fromCurrency, toCurrency) => {
   const response = await axios.get(`http://data.fixer.io/api/latest?access_key=${myKey}&format=1`);
