350e07479e9d6cd205192ba6511928f52a90edb4
Tony Kieling <tony.kieling@gmail.com>
2019-05-19 09:04:41 -0700

    adding axios

--- currency.js
+++ currency.js
@@ -1,8 +1,10 @@
 // https://medium.freecodecamp.org/how-to-master-async-await-with-this-real-world-example-19107e7558ad
 
-const myKey = ""
+const myKey = "4200f01792899473248cd9bff4c9c780"
 
 getCurrency = async (fromCurrency, toCurrency) => {
   const response = await axios.get(`http://data.fixer.io/api/latest?access_key=${myKey}&format=1`);
   console.log(response.data.rates)
 }
+
+console.log(axios.get(`http://data.fixer.io/api/latest?access_key=${myKey}&format=1`))
