7a0c062b9f7dd21920fa250984ed3adfe3dd82f7
Tony Kieling <tony.kieling@gmail.com>
2019-05-20 09:30:15 -0700

    changing getCurrency

--- currency.js
+++ currency.js
@@ -1,15 +1,14 @@
+// https://medium.freecodecamp.org/how-to-master-async-await-with-this-real-world-example-19107e7558ad
+
 const axios = require('axios')
 require('dotenv').config()
-// https://medium.freecodecamp.org/how-to-master-async-await-with-this-real-world-example-19107e7558ad
 
 const myKey = process.env.KEY
 
 getCurrency = async (fromCurrency, toCurrency) => {
-  const response = await axios
-    .get(`http://apilayer.net/api/live?access_key=${myKey}&currencies=EUR,GBP,CAD,PLN&source=USD&format=1`);
-  // console.log(response.data.rates)
-  console.log(response.data)
+  const response =  await axios
+    .get(`http://apilayer.net/api/live?access_key=${myKey}&${toCurrency}&source=${fromCurrency}&format=1`);
+    // .get(`http://apilayer.net/api/live?access_key=${myKey}&currencies=EUR,GBP,CAD,PLN&source=USD&format=1`);
 }
 
-// console.log(myKey)
 getCurrency()
