b962320096589e6c7df493a1831b00fa909a77ea
Tony Kieling <tony.kieling@gmail.com>
2019-05-20 14:21:46 -0700

    before new structure

--- getCountry.js
+++ getCountry.js
@@ -1,11 +1,17 @@
+const axios = require('axios')
+let country = "Brazil"
 // const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
 
-// getCountry = async () => {
-//   const response = await axios.get(`https://restcountries.eu/rest/v2/name/brazil`);
-//   return response
-// }
+getCountry = async () => {
+  try {
+    const countryCode = await axios.get(`https://restcountries.eu/rest/v2/name/${country}`);
+    return await countryCode.data[0].currencies[0].code
+  } catch(err) {
+    return err.message
+  }
+}
+// const async countryCode = await getCountry()
+//   .then(country => country.data[0].currencies[0].code)
 
-const cur = require('./getCurrency.js')
-
-cur()
-  .then(r => console.log("r: ",r))
+const x = getCountry().then(res => console.log(res))
+  // console.log("countryCode:: ", x)
--- getCurrency.js
+++ getCurrency.js
@@ -1,6 +1,5 @@
 // https://medium.freecodecamp.org/how-to-master-async-await-with-this-real-world-example-19107e7558ad
 
-'use strict';
 
 const axios = require('axios')
 require('dotenv').config()
