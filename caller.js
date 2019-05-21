1c4cebc10e8d2928b64bf8a9c8111758c7ed81ee
Tony Kieling <tony.kieling@gmail.com>
2019-05-21 09:02:15 -0700

    doing caller.js

--- caller.js
+++ caller.js
@@ -1,5 +1,5 @@
-const getCur = require('./getCurrency.js')
-const getCou = require('./getCountry.js')
+// const getCur = require('./getCurrency.js')
+// const getCou = require('./getCountry.js')
 
 // getCur()
 //   .then(console.log)
@@ -9,5 +9,28 @@ const getCou = require('./getCountry.js')
 
 // const ax = getCou().then()
 
-const ax = Promise.all([getCou('brazil'), getCur()])
-console.log("ax: ", ax)
+// const ax = Promise.all([getCou('brazil'), getCur()])
+// console.log("ax: ", ax)
+
+const axios = require('axios')
+require('dotenv').config()
+const myKey = process.env.KEY
+
+// countryCode.data[0].currencies[0].code
+
+getCountry = async (countryFrom, countryTo) => {
+  try {
+    const countryCodeFrom = await axios.get(`https://restcountries.eu/rest/v2/name/${countryFrom}`);
+    console.log("asd: ", countryCodeFrom.data[0].currencies[0])
+    // console.log("asd: ", countryCodeFrom.data[0].currencies[0].code)
+    // const countryCodeTo = await axios.get(`https://restcountries.eu/rest/v2/name/${countryTo}`);
+    // const res = await axios
+    //   .get(`http://apilayer.net/api/live?access_key=${myKey}&currencies=${countryCodeTo},GBP,CAD,PLN&source=${countryCodeFrom}&format=1`)
+    // console.log("res= ", res)
+    // return await countryCode.data[0].currencies[0].code
+  } catch(err) {
+    return err.message
+  }
+}
+console.log("ji")
+getCountry("brazil", "spain")
--- getCountry.js
+++ getCountry.js
@@ -2,13 +2,15 @@ const axios = require('axios')
 
 // const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
 
-
 // this function returns the code for a passed country
 module.exports = getCountry = async (country) => {
   try {
     const countryCode = await axios.get(`https://restcountries.eu/rest/v2/name/${country}`);
+    console.log(countryCode.data[0].currencies[0].code)
     return await countryCode.data[0].currencies[0].code
   } catch(err) {
     return err.message
   }
 }
+
+getCountry("spain")
