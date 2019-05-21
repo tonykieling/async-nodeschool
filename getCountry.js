cb6f105c8adb23cc18eec11c7132f9aa5a9369ec
Tony Kieling <tony.kieling@gmail.com>
2019-05-21 13:11:37 -0700

    changes due to receive only code from getCountry

--- getCountry.js
+++ getCountry.js
@@ -3,16 +3,25 @@ const axios = require('axios')
 // const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
 
 // this function returns infor about the passed country
-module.exports = getCountry = (country) => {
+module.exports = getCountry = (country, code) => {
+console.log("code== ", code, "country+ ", country)
   return new Promise((res, rej) => {
     try {
-      const countryInfo = axios.get(`https://restcountries.eu/rest/v2/name/${country}`);
-      res(countryInfo)
+      f = async () => {
+
+        const countryInfo = await axios.get(`https://restcountries.eu/rest/v2/name/${country}`);
+        if (code)
+          res(countryInfo.data[0].currencies[0].code)
+        // res(countryInfo.data[0])
+        res(countryInfo)
+      }
+      f()
     } catch(err) {
       rej(err.message)
     }
   })
 }
 
-// getCountry("spain")
-//   .then(country => console.log(country.data[0].capital))
+// getCountry("spain", "code")
+//   .then(console.log)
+  // .then(country => console.log(country.data[0].capital))
--- getCurrency.js
+++ getCurrency.js
@@ -7,13 +7,13 @@ const myKey = process.env.KEY
 const getCountry = require('./getCountry.js')
 
 getCurrency = async (fromCurrency, toCurrency) => {
-  const code1 = await getCountry(fromCurrency)
-  const code2 = await getCountry(toCurrency)
+  const code1 = await getCountry(fromCurrency, "code")
+  const code2 = await getCountry(toCurrency, "code")
 
   return new Promise((res, rej) => {
     try {
       const result = axios
-      .get(`http://apilayer.net/api/live?access_key=${myKey}&currencies=${code1.data[0].currencies[0].code}&source=${code2.data[0].currencies[0].code}&format=1`)
+      .get(`http://apilayer.net/api/live?access_key=${myKey}&currencies=${code1}&source=${code2}&format=1`)
       res(result)
     } catch(err) {
       rej(err.message)
@@ -21,7 +21,7 @@ getCurrency = async (fromCurrency, toCurrency) => {
   });
 }
 // # one way to call, locally
-// getCurrency()
+// getCurrency("brazil", "united")
 //   .then(result => console.log(result.data))
 
 // # another way, as it was called externally
