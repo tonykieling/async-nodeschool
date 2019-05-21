c2bd501362aa3bbc0968fbfb65fbb440299a01b9
Tony Kieling <tony.kieling@gmail.com>
2019-05-21 09:37:12 -0700

    getCurrency formated as Promise which can be exported

--- getCurrency.js
+++ getCurrency.js
@@ -7,16 +7,37 @@ const myKey = process.env.KEY
 
 // this function returns the actual currency convertion from / to
 // it has to receive the countries' (from and to) code
-async function getCurrency(fromCurrency, toCurrency) {
-  try {
-    const res = await axios
-      // .get(`http://apilayer.net/api/live?access_key=${myKey}&currencies=EUR,GBP,CAD&source=USD&format=1`)
-      .get(`http://apilayer.net/api/live?access_key=${myKey}&currencies=CAD&source=USD&format=1`)
-    // return res.data
-    console.log(res.data)
-  } catch(err) {
-    return("err:: ", err)
-  }
+// async function getCurrency(fromCurrency, toCurrency) {
+//   try {
+//     const res = await axios
+//       .get(`http://apilayer.net/api/live?access_key=${myKey}&currencies=CAD&source=USD&format=1`)
+//     // return res.data
+//     console.log(res.data)
+//   } catch(err) {
+//     return("err:: ", err)
+//   }
+// }
+// getCurrency()
+// module.exports = getCurrency
+
+function getCurrency(fromCurrency, toCurrency) {
+  return new Promise((res, rej) => {
+    try {
+      const result = axios.get(`http://apilayer.net/api/live?access_key=${myKey}&currencies=CAD&source=USD&format=1`)
+      res(result)
+    } catch(err) {
+      rej(err.message)
+    }
+  });
 }
-getCurrency()
+// getCurrency()
+//   .then(result => console.log(result.data))
+
+const asd = async() => {
+  const val = await getCurrency()
+  console.log("val: ", val.data)
+}
+
+asd()
+
 module.exports = getCurrency
